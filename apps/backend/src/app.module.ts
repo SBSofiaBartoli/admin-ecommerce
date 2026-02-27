e81e8d66050a70a21419a8dd67c2e894a9f5662d
SofiaBartoli <sofiadbartoli@gmail.com>
2026-02-27 18:41:39 -0300

    Implementacion de service categories y CRUD endpoints

--- apps/backend/src/app.module.ts
+++ apps/backend/src/app.module.ts
@@ -1,7 +1,8 @@
 import { Module } from '@nestjs/common';
 import { PrismaModule } from './prisma/prisma.module';
+import { CategoriesModule } from './modules/categories/categories.module';
 
 @Module({
-  imports: [PrismaModule],
+  imports: [PrismaModule, CategoriesModule],
 })
 export class AppModule {}
