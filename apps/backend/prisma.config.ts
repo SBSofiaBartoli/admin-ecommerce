b1930663d2b9b86ae29261e06838b7a9cbaad6c1
SofiaBartoli <sofiadbartoli@gmail.com>
2026-02-27 22:14:50 -0300

    Arreglo en tsconfig para incluir Prisma config

--- apps/backend/prisma.config.ts
+++ apps/backend/prisma.config.ts
@@ -1,8 +1,7 @@
-/* eslint-disable */
-import path from 'node:path'
-import { defineConfig } from 'prisma/config'
-import { PrismaPg } from '@prisma/adapter-pg'
-import 'dotenv/config'
+import path from 'node:path';
+import { defineConfig } from 'prisma/config';
+import { PrismaPg } from '@prisma/adapter-pg';
+import 'dotenv/config';
 
 export default defineConfig({
   schema: path.join('prisma', 'schema.prisma'),
@@ -11,10 +10,10 @@ export default defineConfig({
   },
   migrate: {
     async adapter() {
-      return new PrismaPg({ connectionString: process.env.DATABASE_URL})
+      return new PrismaPg({ connectionString: process.env.DATABASE_URL });
     },
   },
   datasource: {
     url: process.env.DATABASE_URL,
   },
-})
+});
