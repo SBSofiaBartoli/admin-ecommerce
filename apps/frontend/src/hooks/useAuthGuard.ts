9af919eaf94451060ade0effa39c064bf3887db2
SofiaBartoli <sofiadbartoli@gmail.com>
2026-02-28 22:26:41 -0300

    Creacion de authGuard

---
+++ apps/frontend/src/hooks/useAuthGuard.ts
@@ -0,0 +1,16 @@
+"use client";
+
+import { useEffect } from "react";
+import { useRouter } from "next/navigation";
+
+export function useAuthGuard() {
+  const router = useRouter();
+
+  useEffect(() => {
+    const token = localStorage.getItem("access_token");
+
+    if (!token) {
+      router.replace("/login");
+    }
+  }, [router]);
+}
